9ab1bc16ab2009f9403c4da02ac8b1192f9223d9
prince2312yadav <47516627+prince2312yadav@users.noreply.github.com>
2019-10-15 00:53:54 +0530

    c program to find fibonacci series

---
+++ Mathematical/fib2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int F[10];
+int fib(int n)
+{
+	if(n<=1)
+	{
+		F[n]=n;
+		return n;
+	}
+	else
+	{
+		if(F[n-2]=-1)
+		F[n-2]=fib(n-2);
+		if(F[n-1]=-1)
+		F[n-1]=fib(n-1);
+		return F[n-2]+F[n-1];
+	}
+}
+void main()
+{
+	int i=0;
+	for(i=0;i<10;i++)
+	{
+		F[i]=-1;
+	}
+	printf("%d ",fib(10));
+}
